239c75abb857c38b1b4be42d9ab8a1acb7093dd6
Grig Alex <alexengrigdev@gmail.com>
2019-09-14 13:22:06 +0300

    Add message bundle

---
+++ src/main/java/io/github/alexengrig/lambdax/intellij/plugin/message/MessageBundle.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2019 LambdaX IntelliJ plugin contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.alexengrig.lambdax.intellij.plugin.message;
+
+import com.intellij.CommonBundle;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.PropertyKey;
+
+import java.util.ResourceBundle;
+
+public class MessageBundle {
+    @NonNls
+    private static final String BUNDLE_NAME = "messages.pluginBundle";
+    private static final ResourceBundle BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
+
+    private MessageBundle() {
+    }
+
+    public static String message(@PropertyKey(resourceBundle = BUNDLE_NAME) String key, Object... params) {
+        return CommonBundle.message(BUNDLE, key, params);
+    }
+}
