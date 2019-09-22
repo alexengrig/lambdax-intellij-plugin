bfdb3ad2e890d6e06cea3cfcb44347b3c4983262
Grig Alex <alexengrigdev@gmail.com>
2019-09-22 23:10:33 +0300

    Add test data after for MapGetInspection

---
+++ src/test/resources/inspection/collection/mapGet.after.java
@@ -0,0 +1,7 @@
+public class X {
+    public void x() {
+        java.lang.Object value = java.util.Optional.of(new java.util.HashMap<java.lang.Object, java.lang.Object>())
+                .map(MapX.get(1))
+                .orElse(0);
+    }
+}
